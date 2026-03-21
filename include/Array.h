fa2793766d43e158056bc08f121f8fad1db32fca
Irina Andrieieva <287561@student.pwr.edu.pl>
2026-03-21 21:30:11 +0100

    Add add() method to Array.h

--- include/Array.h
+++ include/Array.h
@@ -4,6 +4,7 @@
 
 #ifndef ARRAY_H
 #define ARRAY_H
+#include <iostream>
 
 // Dynamiczna tablica, która jest inicjalizowana jako szablon
 template<class T>
@@ -13,15 +14,27 @@ private:
     T *data;
 
 public:
+
     explicit Array(int size) {
         if (size > 0) {
             data = new T[size]{};
             array_size = size;
+        } else {
+            array_size = 0;
+            throw std::invalid_argument("Array size must be greater than 0");
         }
     }
 
     ~Array() {
         delete[] data;
+    }
+
+    void add(int index, const T& value) {
+        if (index >= array_size || index < 0) {
+            throw std::invalid_argument("Index out of range");
+        }else{
+            data[index] = value;
+        }
     }
 };
 
