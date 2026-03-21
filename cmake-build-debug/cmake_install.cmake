0e6c0b6b5be3f89ca74db5ef2cb0871ddedc519e
Irina Andrieieva <287561@student.pwr.edu.pl>
2026-03-21 21:10:12 +0100

    Add push method and Node constructor to SinglyLinkedList.h

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/iraan/projekt-algorytmy
+# Install script for directory: C:/Users/iraan/algorithm-project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/iraan/projekt-algorytmy/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/iraan/algorithm-project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- include/SinglyLinkedList.h
+++ include/SinglyLinkedList.h
@@ -3,22 +3,36 @@
 //
 #ifndef SINGLYLINKEDLIST_H
 #define SINGLYLINKEDLIST_H
+#include <iterator>
+#include <bits/range_access.h>
 
 // Dynamiczna lista jednokierunkowa,  która jest inicjalizowana jako szablon
 template<class T>
 class SinglyLinkedList {
+
 private:
-    struct Node {
+    struct Node  {
         T data;
         Node *next;
+
+        //konstruktor węzła do przechowywania w liście
+        Node(T d) {
+            data = d;
+            next = nullptr;
+        }
     };
+
     Node *head;
+    int size;
 
 public:
+    //konstruktor listy
     SinglyLinkedList () {
         head = nullptr;
+        size = 0;
     }
 
+    //destruktor listy
     ~SinglyLinkedList () {
         Node *current = head;
         while (current != nullptr) {
@@ -26,6 +40,21 @@ public:
             delete current;
             current = next;
         }
+    }
+
+    //metoda która dodaje nową wartość do listy
+    void push(T d) {
+        Node *newNode = new Node(d);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node *current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+
     }
 
 };
