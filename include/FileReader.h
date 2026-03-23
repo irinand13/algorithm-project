9c67544304ba3b0026a344c9721f305d02654567
Irina Andrieieva <287561@student.pwr.edu.pl>
2026-03-23 21:19:08 +0100

    Add FileReader class with template array loading.

--- include/FileReader.h
+++ include/FileReader.h
@@ -1,16 +1,8 @@
 //
-// Created by iraan on 22.03.2026.
+// Created by iraan on 23.03.2026.
 //
 
 #ifndef FILEREADER_H
 #define FILEREADER_H
-
-
-
-class FileReader {
-
-};
-
-
 
 #endif //FILEREADER_H
